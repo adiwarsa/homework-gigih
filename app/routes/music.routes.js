a9926445ef24613302b8a69cb2f590c432b34f93
adiwarsa <adiwarsa03@gmail.com>
2023-07-15 18:57:31 +0800

    fix route for mostplayed

--- app/routes/music.routes.js
+++ app/routes/music.routes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const music = require('../controllers/music.controller');
 
 router.get('/', music.findAll);
+router.get('/most-played', music.mostPlayed);
 router.get('/:id', music.show);
 router.post('/', music.create);
 router.put('/:id', music.update);
 router.delete('/:id', music.delete);
-router.get('/most-played', music.mostPlayed);
+
 
 module.exports = router;
